b80ae7e8ae1c24ba2210681d33cbd8401f5b4073
sunz8281 <sunz8281@gmail.com>
2024-06-17 02:21:45 +0900

    2nd commit

--- php/index.php
+++ php/index.php
@@ -1,22 +1,24 @@
 <html>
     <head>
-        <link rel="stylesheet" href="index.css";>
+        <link rel="stylesheet" href="../css/index.css">
         <title>NAVER</title>
     </head>
 <body>
     <div id = header>
         <div id = search>
-            <div>
+            <div id = search_box1>
                 <form method=post action=search.php>
-                    <a href=index.php><img src=image/Logo_N2.png alt=logo class=search_logo></a>
+                    <a href=index.php><img src=../image/Logo_N2.png alt=logo class=search_logo></a>
                     <input type = text name = search_text id=search_text placeholder="검색어를 입력해 주세요.">
                     <input type = button name = search_keyboard id=keyboardpop class=search_logo value="">
-                    <img src=image/autocomplete.png alt=자동완성 height="5px" id=autoComplete>
-                    <input type = image name = search_submit class=search_logo src=image/Logo_search2.png alt=검색>
+                    <img src=../image/autocomplete.png alt=자동완성 height="5px" id=autoComplete>
+                    <input type = image name = search_submit class=search_logo src=../image/Logo_search2.png alt=검색>
                 </form>
             </div>
-            <div>
-                
+            <div id = search_box2>
+                <div class = autoSection>자동완성 끄기</div><div class=dot>&middot;</div>
+                <div class = autoSection>도움말</div><div class=dot>&middot;</div>
+                <div class = autoSection>신고</div>
             </div>
         </div>
     <div class="keyboard">
@@ -32,12 +34,17 @@
 <script>
     const $keyboardPop = document.getElementById('keyboardpop');
     const keyboard = document.querySelector(".keyboard");
+    const $AutoComp = document.getElementById('autoComplete');
+    const $search = document.getElementById('search');
+    const $sBox1 = document.getElementById('search_box1');
+    const $sBox2 = document.getElementById('search_box2');
     let lastX = 0;
     let lastY = 0; 
     let startX = 0; 
     let startY = 0; 
 
     keyboard.style.display = 'none';
+    $sBox2.style.display = 'none';
     $keyboardPop.addEventListener('click', popUp);
     function popUp(){
         if(keyboard.style.display=='block')
@@ -49,6 +56,23 @@
             keyboard.style.left ='1100px';
         }
     }
+    $AutoComp.addEventListener('click', AC);
+    function AC(){
+        if($sBox2.style.display=='none'){
+            $sBox2.style.display='flex';
+            $AutoComp.style.transform = 'scaleY(-1)';
+            $sBox1.style.border='none';
+            $search.style.boxShadow='0px 7px 5px rgba(0, 0, 0, 0.1), 0px 0px 3px rgba(0,0,0,0.1)';
+            $sBox1.style.borderRadius='10px 10px 0px 0px';
+        } else {
+            $sBox2.style.display='none';
+            $AutoComp.style.transform = 'scaleY(1)';
+            $sBox1.style.border='1px solid #03C75A';
+            $search.style.boxShadow='none';
+            $sBox1.style.borderRadius='50px';
+        }
+    }
+
     keyboard.addEventListener('mousedown', function(e){
     e.preventDefault(); 
     startX = e.clientX; 
